167c62d6d4e0b8d09641a10dc1255009ea172a95
Christofer Bäcklin <christofer.backlin@formulate.se>
2018-05-24 08:09:33 +0200

    benchmark.sh: update according to JStatSoft changes

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ $# != 2 ]; then
     echo "Usage ./benchmark.sh <R-script> <title>"
     exit 0
@@ -8,5 +10,5 @@ fi
 [ -f $2.ps.log ] && rm $2.ps.log
 [ -f $2.ps.raw ] && rm $2.ps.raw
 
-../syrupy.py --no-raw-process-log --separator=, --no-align --title=$2 R --vanilla -f $1
+$DIR/syrupy.py --no-raw-process-log --separator=, --no-align --title=$2 R --vanilla -f $1
 
